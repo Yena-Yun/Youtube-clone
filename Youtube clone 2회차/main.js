7a62e5a96079aa182dfad0a1d8a136c1339d9cd0
Yena-Yun <cheryleduart@gmail.com>
2021-08-24 21:07:12 +0900

    action button toggle

--- Youtube clone 2회차/main.js
+++ Youtube clone 2회차/main.js
@@ -5,3 +5,38 @@ moreBtn.addEventListener("click", () => {
   moreBtn.classList.toggle("clicked");
   title.classList.toggle("clamp");
 });
+
+const likesBtn = document.querySelector(".actions .likes i");
+console.log(likesBtn);
+
+const dislikesBtn = document.querySelector(".actions .dislikes i");
+console.log(dislikesBtn);
+
+const shareBtn = document.querySelector(".actions .share i");
+console.log(likesBtn);
+
+const saveBtn = document.querySelector(".actions .save i");
+console.log(saveBtn);
+
+const reportBtn = document.querySelector(".actions .report i");
+console.log(reportBtn);
+
+likesBtn.addEventListener("click", () => {
+  likesBtn.classList.toggle("active");
+});
+
+dislikesBtn.addEventListener("click", () => {
+  dislikesBtn.classList.toggle("active");
+});
+
+shareBtn.addEventListener("click", () => {
+  shareBtn.classList.toggle("active");
+});
+
+saveBtn.addEventListener("click", () => {
+  saveBtn.classList.toggle("active");
+});
+
+reportBtn.addEventListener("click", () => {
+  reportBtn.classList.toggle("active");
+});
